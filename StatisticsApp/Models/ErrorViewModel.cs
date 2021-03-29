17c36ca01490082c0642697e9883e7d52f68bdac
Paula <paula.hercigonja@fer.hr>
2021-03-29 13:21:43 +0200

    Add project files.

---
+++ StatisticsApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StatisticsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
