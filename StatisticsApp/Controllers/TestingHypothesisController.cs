11ee6a43a6e397dcbb43e1c4250f244df0d287dd
Paula <paula.hercigonja@fer.hr>
2021-05-23 12:57:35 +0200

    CodeMirror editor for R code

--- StatisticsApp/Controllers/TestingHypothesisController.cs
+++ StatisticsApp/Controllers/TestingHypothesisController.cs
@@ -1,22 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
-using Microsoft.AspNetCore.Hosting.Server;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace StatisticsApp.Controllers
 {
-    public class UploadController : Controller
+    public class TestingHypothesisController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
-
-
     }
 }
